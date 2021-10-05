aa20f88dc158d632289118f70e75d251b3a12885
menashekoren <menashekoren@gmail.com>
2021-10-05 08:29:17 +0300

    Feat: New Updates for heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4+!#g2afi5o$so@*_ccw-t@w1k8(*j#0m#$sjx&xm5ij1=#v20
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'localhost']
 
 
 # Application definition
