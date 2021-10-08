dfe9a1e1748c9327cfcf41fe235ee42610774c3d
menashekoren <menashekoren@gmail.com>
2021-10-08 17:08:20 +0300

    Add intents.json, training.py, and chatbot.py(wip)

---
+++ chatbot/chatbot.py
@@ -0,0 +1,14 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import load_model()
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open)
